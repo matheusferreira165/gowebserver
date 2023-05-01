963684d15e67e568bbc428947317ad1200f91fa4
Mathus Ferreira <matheus.ferreira165@outlook.com>
2023-05-01 14:13:17 -0300

    Hello World web server

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+	http.ListenAndServe(":8080", nil)
 }
